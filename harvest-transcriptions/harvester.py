586eb567577830869c4532701949abfe790a3335
thibault-roux <thibault.roux1@laposte.net>
2022-11-22 17:13:04 +0100

    multi metrics taken account

--- harvest-transcriptions/harvester.py
+++ harvest-transcriptions/harvester.py
@@ -5,8 +5,8 @@ import argparse
 parser = argparse.ArgumentParser(description="Allows to extract interesting sentence")
 parser.add_argument("sys1", type=str, help="Name of the first system.")
 parser.add_argument("sys2", type=str, help="Name of the second system.")
-parser.add_argument("metric", type=str, help="Name of the second system.")
-parser.add_argument("difference", type=str, help="Name of the second system.")
+# parser.add_argument("metric", type=str, help="Name of the second system.")
+# parser.add_argument("difference", type=str, help="Name of the second system.")
 args = parser.parse_args()
 
 
@@ -19,24 +19,79 @@ def removeEPS(sentence):
     return ' '.join(new_sentence_list)
 
 
-
-
-def get_score(metric, sys): # return a dictionary -> dico[sys] = [score, ref, hyp]
+def get_score(sys): # return a dictionary -> dico[id] = [ref, hyp, wer, cer, ember, semdist]
     dico = dict()
-    with open("results/correlation/" + metric + "_" + sys + ".txt", "r", encoding="utf8") as file: # ID to score
+    with open("data/" + sys + "/" + sys + "1.txt", "r", encoding="utf8") as file: # Retrieve id, references, hypothesis
         for ligne in file:
             ligne = ligne[:-1].split("\t")
-            dico[int(ligne[0])] = [float(ligne[1]), "<EMPTY>", "<EMPTY>"]
-    with open("data/" + sys + "/" + sys + "1.txt", "r", encoding="utf8") as file: # ID to ref and hyp in the same dico
-        for ligne in file:
-            ligne = ligne[:-1].split("\t")
-            dico[int(ligne[0])] = [dico[int(ligne[0])][0], removeEPS(ligne[1]), removeEPS(ligne[2])]
-    return dico # dico[id] = [score, ref, hyp]
+            id = ligne[0]
+            ref = removeEPS(ligne[1])
+            hyp = removeEPS(ligne[2])
+            dico[id] = dict()
+            dico[id]["ref"] = ref # store reference
+            dico[id]["hyp"] = hyp # store hypothesis
+            
+    for metric in ["wer", "cer", "ember", "semdist"]:
+        with open("results/correlation/" + metric + "_" + sys + ".txt", "r", encoding="utf8") as file: # Retrieve score for each metric
+            for ligne in file:
+                ligne = ligne[:-1].split("\t")
+                id = ligne[0]
+                score = str(float(ligne[1]))
+                dico[id][metric] = score
+
+    return dico # dico[id] = [ref, hyp, wer, cer, ember, semdist] # dict of dict
 # NB: Scores in correlation correspond to real score computed from data. (One exception catch on 5437 for WER)
 
 
-sys1 = get_score(args.metric, args.sys1)
-sys2 = get_score(args.metric, args.sys2)
+#sys1 = get_score(args.sys1)
+#sys2 = get_score(args.sys2)
+
+
+def abs(value): # valeur absolue
+    if value < 0:
+        return -value
+    else:
+        return value
+
+def retrieve_transcriptions(sys1, sys2, difference):
+    # ====================================
+    # conditions optionnelles : 
+    # - [one/many] difference relative à une métrique : élevé, faible, nul (identique), peut-être une valeur numérique (exemple: diff(WER) < 10, diff(SemDist) > 20)
+    #               -> trois paramètres : métrique, différence < ou >, valeur de différence (pour l'égalité, faire < 0)
+    # - longueur de la référence
+    # - [one/many] valeur minimum/maximum pour une métrique : Par exemple, le wer doit être au moins à 30% mais inférieur à 70%
+    # ====================================
+    # conditions systématiques à vérifier:
+    # - références identiques
+    # - hypothèses différentes
+
+    for k, v in sys1.items():
+        if sys2[k][1] == v[1]: # si les ref sont identiques pour les deux dictionnaires
+            if sys2[k][2] != v[2]: # si les hypothèses sont différentes
+
+                if sys2[k][0] == v[0]: # le score est le même  # if abs(sys2[k][0] - v[0]) < difference
+                    if len(k.split(" ")) < 15: # longueur maximale de la référence
+                        print(v[0])
+                        print(v[2])
+                        print(sys2[k][2])
+                        print(abs(sys2[k][0] - v[0]))
+                        input()
+                        
+
+
+retrieve_transcriptions(sys1, sys2) #, ["wer<30,semdist=0"])
+
+
+
+# Faire un tableau contenant toutes les valeurs
+# parcourir
+
+
+
+
+
+# =========== TO DELETE =============
+
 
 def check_data(sys1, sys2):
     incoherence = 0
@@ -65,30 +120,3 @@ def check_data(sys1, sys2):
 # Step 2 : calculer le WER des transcriptions (DONE, c'est ok)
 
 #check_data(sys1, sys2)
-
-
-def abs(value): # valeur absolue
-    if value < 0:
-        return -value
-    else:
-        return value
-
-def retrieve_transcriptions(sys1, sys2, difference):
-    for k, v in sys1.items():
-        """if v[0] != 100*wer([v[1]], v[2]): # le WER entre la référence et l'hypothèse correspond à celui enregistré
-            print(k, 100*wer([v[1]], v[2]), v)"""
-        if sys2[k][1] == v[1]: # si les ref sont identiques pour les deux dictionnaires
-            if sys2[k][2] != v[2]: # si les hypothèses sont différentes
-                if sys2[k][0] == v[0]: # le score est le même
-                    if len(k.split(" ")) < 15: # longueur maximale de la référence
-                        print(v[0])
-                        print(v[2])
-                        print(sys2[k][2])
-                        print(abs(sys2[k][0] - v[0]))
-                        input()
-                        # if abs(sys2[k][0] - v[0]) < difference
-
-
-retrieve_transcriptions(sys1, sys2, 0.1)
-
-# GROS SOUCI, ce ne sont toujours pas les bonnes ID dans nos données.
