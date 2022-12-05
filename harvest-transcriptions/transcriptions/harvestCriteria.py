d351aa37e8f039dfbf6b6b3f46ef83c6d9f634ce
thibault-roux <thibault.roux1@laposte.net>
2022-12-05 18:47:39 +0100

    update

--- harvest-transcriptions/transcriptions/harvestCriteria.py
+++ harvest-transcriptions/transcriptions/harvestCriteria.py
@@ -1,30 +1,74 @@
+import random
+
+if __name__ == "__main__":
+    with open("crits.txt", "r", encoding="utf8") as file:
+        crits = set()
+        for ligne in file:
+            crits.add(ligne[:-1])
+        
+    with open("syscomp.txt", "r", encoding="utf8") as file:
+        syscomp = set()
+        for ligne in file:
+            syscomp.add(ligne[:-1])
+
+    # put all transcriptions in RAM
+    All = dict()
+    for sysc in syscomp:
+        All[sysc] = dict()
+        for crit in crits:
+            zeset = set()
+            with open("mess/" + sysc + "/" + crit, "r", encoding="utf8") as file:
+                for ligne in file:
+                    zeset.add(ligne[:-1])
+            All[sysc][crit] = zeset
 
 
+        
+    while True:
+        Sample = set() # sample of all transcriptions
+        satisfy = dict() # to get the number of pair that satisfy each criteria
+        for crit in crits:
+            satisfy[crit] = 0 # initialize all criteria to zero
+        for sysc in syscomp:
+            satisfy[sysc] = 0 # initialize all criteria to zero
 
-with open("crits.txt", "r", encoding="utf8") as file:
-    crits = []
-    for ligne in file:
-        crits.append(ligne[:-1])
-    
-with open("syscomp.txt", "r", encoding="utf8") as file:
-    syscomp = []
-    for ligne in file:
-        syscomp.append(ligne[:-1])
+        while len(Sample) < 1500:
+            while True: # select a pair not already in our sample
+                rand_sysc = random.sample(syscomp, 1)[0]
+                rand_crit = random.sample(crits, 1)[0]
+                if len(All[rand_sysc][rand_crit]) >= 1:
+                    pair = random.sample(All[rand_sysc][rand_crit], 1)[0]
+                if pair not in Sample:
+                    break
+            Sample.add(pair)
 
-# put all transcriptions in RAM
-for sys in syscomp:
-    for crit in crits:
-        with open()
 
-Sample = set() # sample of all transcriptions
+            syscrits = set()
+            for sysc in syscomp:
+                for crit in crits:
+                    if pair in All[sysc][crit]:
+                        syscrits.add(sysc)
+                        syscrits.add(crit)
+            for syscrit in syscrits:
+                satisfy[syscrit] += 1
 
-# je peux prendre une transcription et voir combien de critère elle satisfait
-# sinon, je prends une transcription de façon aléatoire pour nos 1485 types de comparaisons.
-# je l'ajoute, ce qui fait augmenter le nombre d'itération de notre critère (on s'en fiche si la transcription satisfait plusieurs critères)
-# j'ai peur que certains critères soient sureprésentés. Pour les statistiques, cela pose problème aussi. On ne sait pas quel pourcentage de chaque critère on a dans notre dataset.
-# je veux qu'on sache que dans notre dataset de 1000 paires, on a 30 paires qui satisfaient le critère KD_woR-KD-wR ; on a 115 paires qui satisfaient w=
+        unsatisfied = []
+        for k, v in satisfy.items():
+            if v < 50:
+                # print(k, v)
+                unsatisfied.append(int(v))
+        #print("mean:", sum(unsatisfied)/len(unsatisfied)) #maximiser moyenne
+        print("len: ", len(unsatisfied)) # minimiser len
+        if len(unsatisfied) < 10:
+            for k, v in satisfy.items():
+                if v < 50:
+                    print(k, v)
+            ans = input("Is it okay? y/n : ")
+            if ans == "y":
+                break
 
-# Il y a énormémement de paires qui sont dans plusieurs critères à la fois.
-
-# Pour la statisticité, je veux que pour chaque paire choisie, on regarde pour chaque critère
-
+    with open("autoselect.txt", "w", encoding="utf8") as file:
+        txt = ""
+        for line in Sample:
+            txt += line + "\n"
+        file.write(txt)
