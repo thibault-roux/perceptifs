e377ae642e799a97e70bd5408746cda860047b11
thibault-roux <thibault.roux1@laposte.net>
2023-06-01 16:00:43 +0200

    Agreement

--- perception-transcriptions/results/rater-agreement.py
+++ perception-transcriptions/results/rater-agreement.py
@@ -1,6 +1,6 @@
 
 
-if __name__ == "__main__":
+def rater_agreement_average():
 
     # read the hats.txt file
     hats = dict()
@@ -35,12 +35,78 @@ if __name__ == "__main__":
         P_bar_e = pA**2 + pB**2
 
         K = (P_bar - P_bar_e)/(1 - P_bar_e)
-        print(K)
         Kappas.append(K)
     print("Average Kappa:", sum(Kappas)/len(Kappas))
 
 
 
+def rater_agreement_full():
+
+    # read the hats.txt file
+    hats = dict()
+    with open("hats.txt", "r", encoding="utf8") as f:
+        id = 1
+        next(f)
+        for line in f:
+            line = line.strip().split("\t")
+            hats[id] = {"A": line[2], "B": line[4]}
+            id += 1
+
+    N = 0 # 850
+    Kappas = []
+    pA = 0
+    pB = 0
+    Pis = []
+    for id in range(20*50):
+        id += 1
+        
+        n = int(hats[id]["A"]) + int(hats[id]["B"]) # 7 most of the times
+        if n != 7:
+            continue
+        A = int(hats[id]["A"])
+        B = int(hats[id]["B"])
+        pA += A
+        pB += B
+        Pi = 1/(n*(n-1)) * (A**2 + B**2 - n)
+        Pis.append(Pi)
+        N += 1
+    pA /= N*7
+    pB /= N*7
+
+    P_bar = sum(Pis)/N
+    P_bar_e = pA**2 + pB**2
+
+    K = (P_bar - P_bar_e)/(1 - P_bar_e)
+    print("Kappa:", K)
 
 
 
+
+def agreement_global():
+    # read the hats.txt file
+    hats = dict()
+    with open("hats.txt", "r", encoding="utf8") as f:
+        id = 1
+        next(f)
+        for line in f:
+            line = line.strip().split("\t")
+            hats[id] = {"A": line[2], "B": line[4]}
+            id += 1
+
+    agreements = []
+    for id in range(20*50):
+        id += 1
+        A = int(hats[id]["A"])
+        B = int(hats[id]["B"])
+
+        agreement = max(A, B) / (A + B)        
+        agreements.append(agreement)
+    print("Average agreement:", sum(agreements)/len(agreements))
+    agreeset = set(agreements)
+    print(agreeset)
+
+if __name__ == "__main__":
+    rater_agreement_average()
+    # rater_agreement_full()
+
+    agreement_global()
