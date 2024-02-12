413fc7c7197670be4dc4d3b7cf4e9d40ee425af3
thibault-roux <thibault.roux1@laposte.net>
2024-02-12 14:34:40 +0100

    generate a dataset of triplets not in hats

---
+++ harvest-transcriptions/generate_new_dataset.py
@@ -0,0 +1,46 @@
+
+
+def load_transcriptions(system):
+    id2refhyp = dict()
+    with open("data/" + system + "/" + system + "1.txt", "r") as f:
+        for line in f:
+            line = line.strip()
+            if line == "":
+                continue
+            line = line.split("\t")
+            id = line[0]
+            ref = line[1]
+            hyp = line[2]
+            id2refhyp[id] = (ref, hyp)
+    return id2refhyp
+
+def load_hats():
+    path = "/users/troux/these/expe/metrics/Hats.txt"
+    hats = set()
+    with open(path, "r", encoding="utf8") as file:
+        next(file)
+        for line in file:
+            line = line.split("\t")
+            ref = line[0]
+            hypA = line[1]
+            hypB = line[3]
+            hats.add((ref, hypA, hypB))
+    return list(hats)
+
+
+# the objective is to produce a dataset containing triplets not present in HATS
+if __name__ == "__main__":
+    systems = ["KD_woR", "KD_wR", "SB_bpe1000", "SB_bpe750", "SB_s2s", "SB_w2v_1k", "SB_w2v_3k", "SB_w2v_7k", "SB_xlsr_fr", "SB_xlsr"]
+
+    data = dict()
+    for system in systems:
+        data[system] = load_transcriptions(system)
+    keys = data[systems[0]].keys()
+    print(keys)
+
+    hats = load_hats()
+
+    # choose a random element from systems
+    import random
+    random_val = random.random(systems)
+
