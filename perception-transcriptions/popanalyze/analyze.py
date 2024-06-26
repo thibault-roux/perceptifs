0c5c72e8c497ac4e55b2e7f0dfd8fa01e84e1575
thibault-roux <thibault.roux1@laposte.net>
2024-06-26 17:33:13 +0200

    Print answers

--- perception-transcriptions/popanalyze/analyze.py
+++ perception-transcriptions/popanalyze/analyze.py
@@ -3,17 +3,17 @@
 # we analyze the choice of humans given 1000 triplets (i.e. a reference, and two hypothesis). Each human evaluate 50 triplets. 
 # The dataset is splitted in 20 sets.
 
-# in folder ../results, there are files in this format:
-#   min_0-331213.json
-#   min_0-523132.json
-#   min_0-762195.json
-#   min_0-378621.json
-#   min_1-123456.json
-#   min_1-234567.json
-#   min_1-345678.json
-#   min_1-456789.json
-#   min_2-465466.json
-#   min_2-654654.json
+# in folder ./data, there are files in this format:
+#   min_0-1.json
+#   min_0-2.json
+#   min_0-3.json
+#   min_0-4.json
+#   min_1-1.json
+#   min_1-2.json
+#   min_1-3.json
+#   min_1-4.json
+#   min_2-1.json
+#   min_2-2.json
 #   ...
 #   min_19-763686.json
 
@@ -29,13 +29,38 @@
 #		 "answers": {"1":"B","2":"B","3":"B","4":"B","5":"B","6":"A","7":"B","8":"A","9":"B","10":"B","11":"B","12":"B","13":"A","14":"A","15":"B","16":"A","17":"B","18":"B","19":"B","20":"B","21":"B","22":"B","23":"A","24":"B","25":"A","26":"A","27":"B","28":"A","29":"A","30":"A","31":"A","32":"A","33":"B","34":"A","35":"A","36":"B","37":"B","38":"A","39":"A","40":"A","41":"A","42":"A","43":"A","44":"B","45":"B","46":"A","47":"A","48":"B","49":"B","50":"A"}}
 
 
-def average_correlation():
 
-    for i in range(20):
-        filename = f"min_{i}-*.json"
-        print(filename)
-        
+import json
 
+
+
+def get_answers(i, j):
+    try:
+        with open(f"./data/min_{i}-{j}.json") as f:
+            data = json.load(f)
+        return data["answers"]
+    except FileNotFoundError:
+        return None
+
+def average_agreement(num_dataset):
+    human2answers = dict()
+    for num_human in range(8):
+        # get answers from json
+        answers = get_answers(num_dataset, num_human)
+        if answers is not None:
+            human2answers[num_human] = answers
+            print(answers)
+            exit()
+
+    for num_human1, answers1 in human2answers.items():
+        for num_human2, answers2 in human2answers.items():
+            if num_human1 != num_human2:
+                # compare answers
+                for k in range(50):
+                    if answers1[k] == answers2[k]:
+                        agreement[k] += 1
+    
 
 if __name__ == "__main__":
-    data = average_correlation()
+    for num_dataset in range(20):
+       data = average_agreement(num_dataset)
